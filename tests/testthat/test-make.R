f2f5e646112bc175e8ce76e49d400307d798481c
Håkon Malmedal <hmalmedal@gmail.com>
2019-07-11 13:20:50 +0200

    Drop silly test

--- tests/testthat/test-make.R
+++ tests/testthat/test-make.R
@@ -1,7 +1,3 @@
-test_that("make gives correct class", {
-  expect_is(make_weeknumber(2000, 1:2), "weeknumber")
-})
-
 test_that("invalid week gives NA", {
   expect_true(is.na(make_weeknumber(2000, 0)))
   expect_true(is.na(make_weeknumber(2000, 53)))
